93b10ca28199f618b521ac4cfa7df4ac68862a58
theCodeMonster1 <amharmohamed@icloud.com>
2025-05-18 08:05:27 +0530

    Remove unused import of 'package:pdf/pdf.dart' in ReportPage

--- lib/screens/report_page.dart
+++ lib/screens/report_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../data/barriers_data.dart';
 import '../utils/score_calculator.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:pdf/pdf.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:open_file/open_file.dart';
 import 'dart:io';
